38f6a3a2726d47d2e8bd3d1417d6de9d3c8552f7
smit1203 <smitmehta077@gmail.com>
2026-04-07 17:06:48 +0530

    phase 1 for removing figma tokens have been done

--- headapps/nextjs-starter/scripts/bootstrap.ts
+++ headapps/nextjs-starter/scripts/bootstrap.ts
@@ -4,13 +4,3 @@
   included into the build - specifically, plugins, the global config module,
   and the component name to component mapping.
 */
-
-/*
-  CSS VARIABLES GENERATION
-*/
-import './figma-tokens/generate-css-variables';
-
-/*
-  BRANDED FILES GENERATION
-*/
-import './generate-branded-files';
